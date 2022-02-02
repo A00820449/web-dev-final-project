6da5784b57be776f8f2fa274373f5497e920cfdf
Miguel Tornero <a00820449@itesm.mx>
2022-02-01 18:14:55 -0600

    added morgan

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
 const PORT = parseInt(process.env.PORT) || 3001;
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/test",(req, res)=>{
