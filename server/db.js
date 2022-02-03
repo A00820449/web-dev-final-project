705a31bf551e4e6d3b82b1df764997b5c0e49d4a
Miguel Tornero <a00820449@itesm.mx>
2022-02-03 10:47:21 -0600

    added authentication logic

--- server/db.js
+++ server/db.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const mongoConnectionString = process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017/test"
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String, unique: true},
     passwordHash: String,
     isAdmin: Boolean,
+});
+userSchema.method("setPassword", async function (password){
+    const hash = await bcrypt.hash(password, 10);
+    this.passwordHash = hash;
+    await this.save();
+    return hash;
+});
+userSchema.method("validatePassword", async function (password){
+    const match = await bcrypt.compare(password, this.passwordHash);
+    return match;
 });
 const User = mongoose.model("User", userSchema);
 
--- server/index.js
+++ server/index.js
@@ -17,8 +17,27 @@ app.get("/test",(req, res)=>{
     res.json({hello: "world"});
 });
 
-app.post("/auth", (req,res)=>{
-    res.json(req.body);
+app.post("/auth", async (req,res)=>{
+    const username = req.body.username.trim() || '';
+    const password = req.body.password.trim() || '';
+
+    if (!username || !password) {
+        return res.status(400).json({error: true, message: "Invalid request"});
+    }
+
+    const count = await User.count({username});
+    if (count !== 1) {
+        return res.status(404).json({error: true, message: "Invalid user"});
+    }
+
+    const user = await User.findOne({username});
+    const match = await user.validatePassword(password);
+
+    if (!match) {
+        return res.status(401).json({error: true, message: "Invalid password"});
+    }
+    
+    return res.json({error: false, message: "Sucess", token: "1234"});
 });
 
 app.listen(PORT, ()=>{
